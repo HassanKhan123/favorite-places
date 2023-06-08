148862219ced5390d7ca040ad0ffd5196faf2282
HassanKhan123 <hk2602809@gmail.com>
2023-06-08 11:32:58 +0500

    added: place details screen

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/screens/places_details.dart';
 import 'package:flutter/material.dart';
 
 import 'package:favorite_places/models/place.dart';
@@ -31,6 +32,13 @@ class PlacesList extends StatelessWidget {
               .titleMedium!
               .copyWith(color: Theme.of(context).colorScheme.onBackground),
         ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailsScreen(place: places[index]),
+            ),
+          );
+        },
       ),
     );
   }
