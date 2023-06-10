b475bfe111c328cc4f8a44fe5651f863bb48c4dc
HassanKhan123 <hk2602809@gmail.com>
2023-06-10 16:43:33 +0500

    added: display address and map on details page

--- lib/screens/places_details.dart
+++ lib/screens/places_details.dart
@@ -6,6 +6,13 @@ class PlaceDetailsScreen extends StatelessWidget {
 
   final Place place;
 
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyB2e5b45Pt-N4maiwWDQsVSYa4Q-ZkEPis';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +27,36 @@ class PlaceDetailsScreen extends StatelessWidget {
               width: double.infinity,
               height: double.infinity,
             ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage(locationImage),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 14, vertical: 16),
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Colors.transparent, Colors.black54],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomRight),
+                    ),
+                    child: Text(
+                      place.location.address,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ));
   }
